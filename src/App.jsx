7f2edb2f439793418b67508cb9db7cde9e3f6011
Sreedhar-roy <sreedharroy0@gmail.com>
2024-10-30 20:58:58 +0600

    first basic component section

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Link from './component/Link/Link'
+
+function App() {
+ 
+  return (
+    <>
+     
+      <h1 className='text-3xl bg-orange-400'>Price Options</h1>
+      <Link></Link>
+    </>
+  )
+}
+
+export default App
